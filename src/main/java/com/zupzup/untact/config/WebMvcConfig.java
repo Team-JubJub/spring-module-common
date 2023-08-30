58fa80ad2a55e9a19ea047afd68a08f0962afd86
seasameoil <whdwlgus12@gmail.com>
2023-08-30 22:50:04 +0900

    add cors

--- src/main/java/com/zupzup/untact/config/WebMvcConfig.java
+++ src/main/java/com/zupzup/untact/config/WebMvcConfig.java
@@ -3,13 +3,19 @@ package com.zupzup.untact.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
-    String uploadPath;
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
 
-
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
 }
--- src/main/java/com/zupzup/untact/domain/store/Store.java
+++ src/main/java/com/zupzup/untact/domain/store/Store.java
@@ -49,6 +49,7 @@ public class Store {
     private Boolean isOpen; // 가게 운영 여부
     @Column(nullable = false)
     private String closedDay; // 휴무일 (0-휴무, 1-영업)
+    private String crNumber; // 사업자 등록 번호
 
     public static StoreBuilder builder(String storeName) {   // 필수 파라미터 고려해볼 것
         if(storeName == null) {
