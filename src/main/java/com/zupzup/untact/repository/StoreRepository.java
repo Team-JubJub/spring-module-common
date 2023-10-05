4e08a8e91aed23a574db8e60025bd3c8c69b62d8
seasameoil <whdwlgus12@gmail.com>
2023-10-05 23:48:35 +0900

    fix: query

--- src/main/java/com/zupzup/untact/repository/StoreRepository.java
+++ src/main/java/com/zupzup/untact/repository/StoreRepository.java
@@ -3,7 +3,7 @@ package com.zupzup.untact.repository;
 
 import com.zupzup.untact.domain.enums.EnterState;
 import com.zupzup.untact.domain.store.Store;
-import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -17,6 +17,6 @@ public interface StoreRepository extends JpaRepository<Store, Long> {
     List<Store> findByCategory(String category);    // 카테고리별 조회
     List<Store> findByStoreNameContaining(String keyword);  // 가게 이름으로 검색
     List<Store> findByEnterState(EnterState enterState); // 등록 상태로 조회
-    @Query("SELECT e FROM Enter e WHERE e.storeName LIKE %:storeName% AND e.state = :state")
+    @Query(value = "SELECT e FROM Enter e WHERE e.storeName LIKE %:storeName% AND e.state = :state", nativeQuery = true)
     List<Store> searchByStoreNameContainingAndEnterState(@Param("storeName") String storeName, @Param("state") EnterState state); // 가게 이름 및 등록 상태로 조회
 }
