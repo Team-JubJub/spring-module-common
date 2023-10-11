7b69a5d8e8b158b8bd81e5c252f08ca62606588d
seasameoil <whdwlgus12@gmail.com>
2023-10-12 03:17:18 +0900

    fix: cors

--- src/main/java/com/zupzup/untact/config/WebMvcConfig.java
+++ src/main/java/com/zupzup/untact/config/WebMvcConfig.java
@@ -1,21 +1,17 @@
 package com.zupzup.untact.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
-
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("*")
-                .allowedHeaders("*");
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**") // CORS를 적용할 URL패턴 정의
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("*") // 허용할 HTTP method 지정
+                .maxAge(3000); // 설정 시간만큼 pre-flight 리퀘스트 캐싱
     }
 }
