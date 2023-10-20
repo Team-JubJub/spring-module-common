6dad5261175dd297b83203d772f27e86c5f93d01
seasameoil <whdwlgus12@gmail.com>
2023-10-20 16:52:11 +0900

    fix: origin 전체 허용

--- src/main/java/com/zupzup/untact/config/WebMvcConfig.java
+++ src/main/java/com/zupzup/untact/config/WebMvcConfig.java
@@ -10,7 +10,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry) {
         corsRegistry.addMapping("/**") // CORS를 적용할 URL패턴 정의
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("*")
                 .allowedMethods("*") // 허용할 HTTP method 지정
                 .maxAge(3000); // 설정 시간만큼 pre-flight 리퀘스트 캐싱
     }
