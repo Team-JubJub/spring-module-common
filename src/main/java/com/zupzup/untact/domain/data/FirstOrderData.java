4fec2b562a3a62be3dec916b77998591034784b8
김영후 <eagle7449@naver.com>
2023-11-12 20:01:22 +0900

    add builder to FirstOrderData

--- src/main/java/com/zupzup/untact/domain/data/FirstOrderData.java
+++ src/main/java/com/zupzup/untact/domain/data/FirstOrderData.java
@@ -1,11 +1,15 @@
 package com.zupzup.untact.domain.data;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity @Getter
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder(builderMethodName = "firstOrderDataBuilder")
 @Table(name = "firstOrderData")
 public class FirstOrderData {
 
@@ -16,5 +20,11 @@ public class FirstOrderData {
 
     @Column(nullable = false) private String registerTime;
     @Column(nullable = false) private String firstOrderTime;
+
+    public static FirstOrderDataBuilder builder(String firstOrderTime) {
+        if (firstOrderTime == null) throw new IllegalArgumentException("필수 파라미터(first order time) 누락");
+
+        return firstOrderDataBuilder().firstOrderTime(firstOrderTime);
+    }
 
 }
