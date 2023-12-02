1010819eb51ff96368af213a4d38a3ed054fd116
김영후 <eagle7449@naver.com>
2023-12-02 19:09:11 +0900

    Stores-Seller joinColumn명 변경

--- src/main/java/com/zupzup/untact/domain/store/Store.java
+++ src/main/java/com/zupzup/untact/domain/store/Store.java
@@ -1,5 +1,6 @@
 package com.zupzup.untact.domain.store;
 
+import com.zupzup.untact.domain.auth.seller.Seller;
 import com.zupzup.untact.domain.enums.EnterState;
 import com.zupzup.untact.domain.enums.StoreCategory;
 import com.zupzup.untact.dto.store.StoreDto;
@@ -24,8 +25,8 @@ public class Store {
     @Getter
     private Long storeId;
 
-    @ManyToOne(optional = false) @JoinColumn(name = "seller")
-    private Long sellerId;
+    @ManyToOne(optional = false) @JoinColumn(name = "sellerId")
+    private Seller seller;
 
     @Column(nullable = false)
     private String storeName; // 가게이름
