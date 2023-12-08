a6a693658ec12e214bf50867fc14bbb696c26e79
seasameoil <whdwlgus12@gmail.com>
2023-12-08 20:35:46 +0900

    add: 찜한 가게 및 알림 설정 한 가게 가져오기

--- src/main/java/com/zupzup/untact/repository/UserRepository.java
+++ src/main/java/com/zupzup/untact/repository/UserRepository.java
@@ -4,6 +4,7 @@ import com.zupzup.untact.domain.auth.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -12,5 +13,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByProviderUserId(String providerUserId); //isPresent() 사용 위해 Optional<> 타입으로 선언
     Optional<User> findByNickName(String nickName);
     Optional<User> findByPhoneNumber(String phoneNumber);
-
+    List<User> findByStarredStoresContains(Long storeId);
+    List<User> findByAlertStoresContains(Long storeId);
 }
