84e618500e614af16951c79dc1f77eed48a96acd
seasameoil <whdwlgus12@gmail.com>
2024-01-09 21:29:29 -0500

    fix: error msg and error status code fix

--- src/main/java/com/zupzup/untact/exception/enter/EnterExceptionType.java
+++ src/main/java/com/zupzup/untact/exception/enter/EnterExceptionType.java
@@ -5,23 +5,23 @@ import org.springframework.http.HttpStatus;
 
 public enum EnterExceptionType implements BaseExceptionType {
 
-    NO_MATCH_ENTER(700, HttpStatus.OK, "해당 id 값을 가진 신청서는 존재하지 않습니다."),
-    SEVER_ERR(707, HttpStatus.EXPECTATION_FAILED, "서버 통신에 실패하였습니다. 다시 시도해주세요.");
+    NO_MATCH_ENTER(HttpStatus.BAD_REQUEST, "해당 id 값을 가진 신청서는 존재하지 않습니다."),
+    SEVER_ERR(HttpStatus.EXPECTATION_FAILED, "서버 통신에 실패하였습니다. 다시 시도해주세요.");
 
-    private int errorCode;
+//    private int errorCode;
     private HttpStatus httpStatus;
     private String errorMessage;
 
-    EnterExceptionType(int errorCode, HttpStatus httpStatus, String errorMessage) {
-        this.errorCode = errorCode;
+    EnterExceptionType(HttpStatus httpStatus, String errorMessage) {
+//        this.errorCode = errorCode;
         this.httpStatus = httpStatus;
         this.errorMessage = errorMessage;
     }
 
-    @Override
-    public int getErrCode() {
-        return this.errorCode;
-    }
+//    @Override
+//    public int getErrCode() {
+//        return this.errorCode;
+//    }
 
     @Override
     public HttpStatus getHttpStatus() {
--- src/main/java/com/zupzup/untact/exception/store/StoreExceptionType.java
+++ src/main/java/com/zupzup/untact/exception/store/StoreExceptionType.java
@@ -5,23 +5,23 @@ import org.springframework.http.HttpStatus;
 
 public enum StoreExceptionType implements BaseExceptionType {
 
-    NO_MATCH_STORE(700, HttpStatus.OK, "해당 id 값을 가진 가게는 존재하지 않습니다."),
-    SEVER_ERR(707, HttpStatus.EXPECTATION_FAILED, "서버 통신에 실패하였습니다. 다시 시도해주세요.");
+    NO_MATCH_STORE(HttpStatus.BAD_REQUEST, "해당 id 값을 가진 가게는 존재하지 않습니다."),
+    SEVER_ERR(HttpStatus.EXPECTATION_FAILED, "서버 통신에 실패하였습니다. 다시 시도해주세요.");
 
-    private int errorCode;
+//    private int errorCode;
     private HttpStatus httpStatus;
     private String errorMessage;
 
-    StoreExceptionType(int errorCode, HttpStatus httpStatus, String errorMessage) {
-        this.errorCode = errorCode;
+    StoreExceptionType(HttpStatus httpStatus, String errorMessage) {
+//        this.errorCode = errorCode;
         this.httpStatus = httpStatus;
         this.errorMessage = errorMessage;
     }
 
-    @Override
-    public int getErrCode() {
-        return this.errorCode;
-    }
+//    @Override
+//    public int getErrCode() {
+//        return this.errorCode;
+//    }
 
     @Override
     public HttpStatus getHttpStatus() {
