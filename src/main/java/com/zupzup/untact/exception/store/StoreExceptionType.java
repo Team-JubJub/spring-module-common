480d6d241945901b7ad1cc130780a73ea273ad46
seasameoil <whdwlgus12@gmail.com>
2024-06-12 16:33:51 +0900

    store exception errMsg 수정

--- src/main/java/com/zupzup/untact/exception/store/StoreExceptionType.java
+++ src/main/java/com/zupzup/untact/exception/store/StoreExceptionType.java
@@ -5,7 +5,7 @@ import org.springframework.http.HttpStatus;
 
 public enum StoreExceptionType implements BaseExceptionType {
 
-    NO_MATCH_STORE(HttpStatus.BAD_REQUEST, "해당 id 값을 가진 가게는 존재하지 않습니다."),
+    NO_MATCH_STORE(HttpStatus.BAD_REQUEST, "Store with provided unique ID doesn't present."),
     CANNOT_APPLY_TWICE(HttpStatus.BAD_REQUEST, "가게 입점 신청은 한 번만 가능합니다."),
     SEVER_ERR(HttpStatus.EXPECTATION_FAILED, "서버 통신에 실패하였습니다. 다시 시도해주세요.");
 
